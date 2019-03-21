a9fcb39224713daa4ef019104b817446c915ca92
EvanXzj <2686465170@qq.com>
2019-03-21 10:06:12 +0800

    添加文档注释

--- string.go
+++ string.go
@@ -1,5 +1,7 @@
+// Pacakge common contains some commonly used functions for the Go programming language.
 package common
 
+// Reverse a string, support unicode.
 func Reverse(s string) string {
 	r := []rune(s)
 	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
